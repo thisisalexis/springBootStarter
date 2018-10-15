019f418dfa3c2c4316bd59e75d9f6f5a031def25
= <=>
2018-10-15 20:05:23 +0200

    First commit. Spring boot application added

---
+++ src/main/java/ve/com/thisisalexis/sprintstarter/SprintstarterApplication.java
@@ -0,0 +1,12 @@
+package ve.com.thisisalexis.sprintstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintstarterApplication.class, args);
+	}
+}
---
+++ src/test/java/ve/com/thisisalexis/sprintstarter/SprintstarterApplicationTests.java
@@ -0,0 +1,16 @@
+package ve.com.thisisalexis.sprintstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprintstarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
